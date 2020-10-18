6e9ed3bf4fabcd882fa2dabcfb047fa41fe974d3
Alex Bates <hi@imalex.xyz>
2020-10-18 15:02:05 +0100

    update fs.json format

--- segtypes/PaperMarioMapFS.py
+++ segtypes/PaperMarioMapFS.py
@@ -24,27 +24,32 @@ class N64SegPaperMarioMapFS(N64Segment):
 
         filesystem = {
             "title": decode_null_terminated_ascii(data[0:0x20]),
-            "files": [],
+            "assets": [],
         }
 
         asset_idx = 0
         while True:
             asset_data = data[0x20 + asset_idx * 0x1C :]
 
-            name = decode_null_terminated_ascii(asset_data[0:])
-            if name == "end_data":
-                break
-
+            name              = decode_null_terminated_ascii(asset_data[0:])
             offset            = int.from_bytes(asset_data[0x10:0x14], byteorder="big")
             size              = int.from_bytes(asset_data[0x14:0x18], byteorder="big")
             decompressed_size = int.from_bytes(asset_data[0x18:0x1C], byteorder="big")
 
             is_compressed = size != decompressed_size
 
-            path = paths.get(name, "{}.bin".format(name))
-            Path(fs_dir, path).parent.mkdir(parents=True, exist_ok=True)
+            if offset == 0:
+                path = None
+            else:
+                path = paths.get(name, "{}.bin".format(name))
+                Path(fs_dir, path).parent.mkdir(parents=True, exist_ok=True)
 
-            filesystem["files"].append({ "path": path, "compress": is_compressed })
+            print(f"{name} {offset:08X} {size:08X} {decompressed_size:08X}")
+
+            if name == "end_data":
+                break
+
+            filesystem["assets"].append({ "name": name, "path": path, "compress": is_compressed })
 
             with open(os.path.join(fs_dir, path), "wb") as f:
                 bytes = rom_bytes[self.rom_start + 0x20 + offset : self.rom_start + 0x20 + offset + size]
