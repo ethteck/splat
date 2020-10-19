66f07b7adeae38ee5f939d63f140d70f80fb2713
Alex Bates <hi@imalex.xyz>
2020-10-19 03:02:53 +0100

    fix --modes again

--- segtypes/PaperMarioMapFS.py
+++ segtypes/PaperMarioMapFS.py
@@ -19,52 +19,52 @@ class N64SegPaperMarioMapFS(N64Segment):
         self.paths = segment.get("paths", {})
 
     def split(self, rom_bytes, base_path):
-        fs_dir = Path(base_path, self.name)
-        fs_dir.mkdir(parents=True, exist_ok=True)
+        if self.type in self.options["modes"] or "all" in self.options["modes"]:
+            fs_dir = self.create_split_dir(base_path, self.name)
 
-        data = rom_bytes[self.rom_start : self.rom_end]
+            data = rom_bytes[self.rom_start : self.rom_end]
 
-        filesystem = {
-            "title": decode_null_terminated_ascii(data[0:0x20]),
-            "assets": [],
-        }
+            filesystem = {
+                "title": decode_null_terminated_ascii(data[0:0x20]),
+                "assets": [],
+            }
 
-        asset_idx = 0
-        while True:
-            asset_data = data[0x20 + asset_idx * 0x1C :]
+            asset_idx = 0
+            while True:
+                asset_data = data[0x20 + asset_idx * 0x1C :]
 
-            name              = decode_null_terminated_ascii(asset_data[0:])
-            offset            = int.from_bytes(asset_data[0x10:0x14], byteorder="big")
-            size              = int.from_bytes(asset_data[0x14:0x18], byteorder="big")
-            decompressed_size = int.from_bytes(asset_data[0x18:0x1C], byteorder="big")
+                name              = decode_null_terminated_ascii(asset_data[0:])
+                offset            = int.from_bytes(asset_data[0x10:0x14], byteorder="big")
+                size              = int.from_bytes(asset_data[0x14:0x18], byteorder="big")
+                decompressed_size = int.from_bytes(asset_data[0x18:0x1C], byteorder="big")
 
-            is_compressed = size != decompressed_size
+                is_compressed = size != decompressed_size
 
-            if offset == 0:
-                path = None
-            else:
-                path = self.paths.get(name, "{}.bin".format(name))
-                Path(fs_dir, path).parent.mkdir(parents=True, exist_ok=True)
-
-            print(f"{name} {offset:08X} {size:08X} {decompressed_size:08X}")
-
-            if name == "end_data":
-                break
-
-            filesystem["assets"].append({ "name": name, "path": path, "compress": is_compressed })
-
-            with open(os.path.join(fs_dir, path), "wb") as f:
-                bytes = rom_bytes[self.rom_start + 0x20 + offset : self.rom_start + 0x20 + offset + size]
-                if is_compressed:
-                    decompressed_bytes = Yay0decompress.decompress_yay0(bytes)
-                    f.write(decompressed_bytes)
+                if offset == 0:
+                    path = None
                 else:
-                    f.write(bytes)
+                    path = self.paths.get(name, "{}.bin".format(name))
+                    Path(fs_dir, path).parent.mkdir(parents=True, exist_ok=True)
 
-            asset_idx += 1
+                print(f"{name} {offset:08X} {size:08X} {decompressed_size:08X}")
 
-        with open(os.path.join(base_path, "{}.json".format(self.name)), "w") as f:
-            json.dump(filesystem, f, indent=4)
+                if name == "end_data":
+                    break
+
+                filesystem["assets"].append({ "name": name, "path": path, "compress": is_compressed })
+
+                with open(os.path.join(fs_dir, path), "wb") as f:
+                    bytes = rom_bytes[self.rom_start + 0x20 + offset : self.rom_start + 0x20 + offset + size]
+                    if is_compressed:
+                        decompressed_bytes = Yay0decompress.decompress_yay0(bytes)
+                        f.write(decompressed_bytes)
+                    else:
+                        f.write(bytes)
+
+                asset_idx += 1
+
+            with open(os.path.join(base_path, "{}.json".format(self.name)), "w") as f:
+                json.dump(filesystem, f, indent=4)
 
 
     def get_ld_section(self):
