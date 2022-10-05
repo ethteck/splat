575df797b3bc16196ea5a8d8cfc0766d5f160509
Dylan Ascencio <empressglatia@gmail.com>
2022-10-04 23:41:17 -0400

    Add relheader

---
+++ segtypes/gc/relheader.py
@@ -0,0 +1,116 @@
+from segtypes.common.header import CommonSegHeader
+from util import options
+
+
+class RelSegHeader(CommonSegHeader):
+    def parse_header(self, dol_bytes, version):
+        header_lines = []
+        header_lines.append(".section .data\n")
+        
+        # Module ID
+        header_lines.append(
+            self.get_line("word", [0x00:0x04], "Module ID")
+        )
+        
+        # Next module (filled at runtime)
+        header_lines.append(
+            self.get_line("word", [0x04:0x08], "Next Module")
+        )
+        # Last module (filled at runtime)
+        header_lines.append(
+            self.get_line("word", [0x08:0x0C], "Last Module")
+        )
+        
+        # Section count
+        header_lines.append(
+            self.get_line("word", [0x0C:0x10], "Section Count")
+        )
+        # Section table offset
+        header_lines.append(
+            self.get_line("word", [0x10:0x14], "Section Table Offset")
+        )
+        
+        # Module name offset (might be null)
+        header_lines.append(
+            self.get_line("word", [0x14:0x18], "Module Name Offset")
+        )
+        # Module name length
+        header_lines.append(
+            self.get_line("word", [0x18:0x1C], "Module Name Length")
+        )
+        
+        # REL format version
+        header_lines.append(
+            self.get_line("word", [0x1C:0x20], "REL Format Version")
+        )
+        
+        # BSS size
+        header_lines.append(
+            self.get_line("word", [0x20:0x24], "BSS Size")
+        )
+        
+        # Relocation table offset
+        header_lines.append(
+            self.get_line("word", [0x24:0x28], "Relocation Table Offset")
+        )
+        # Import table offset
+        header_lines.append(
+            self.get_line("word", [0x28:0x2C], "Import Table Offset")
+        )
+        # Import table size
+        header_lines.append(
+            self.get_line("word", [0x2C:0x30], "Import Table Size")
+        )
+        
+        # Prolog section index
+        header_lines.append(
+            self.get_line("byte", [0x30:0x31], "Prolog Section Index")
+        )
+        # Epilog section index
+        header_lines.append(
+            self.get_line("byte", [0x31:0x32], "Epilog Section Index")
+        )
+        # Unresolved section index
+        header_lines.append(
+            self.get_line("byte", [0x32:0x33], "Unresolved Section Index")
+        )
+        # BSS section index
+        header_lines.append(
+            self.get_line("byte", [0x33:0x34], "BSS Section Index")
+        )
+        
+        # Prolog function offset
+        header_lines.append(
+            self.get_line("word", [0x34:0x38], "Prolog Function Offset")
+        )
+        # Epilog function offset
+        header_lines.append(
+            self.get_line("word", [0x38:0x3C], "Epilog Function Offset")
+        )
+        # Unresolved function offset
+        header_lines.append(
+            self.get_line("word", [0x3C:0x40], "Unresolved Function Offset")
+        )
+        
+        # Version 1 is only 0x40 bytes long
+        if version <= 1:
+            return
+            
+        # Alignment constraint
+        header_lines.append(
+            self.get_line("word", [0x40:0x44], "Alignment Constraint")
+        )
+        # BSS alignment constraint
+        header_lines.append(
+            self.get_line("word", [0x44:0x48], "BSS Alignment Constraint")
+        )
+        
+        # Version 2 is only 0x48 bytes long
+        if version <= 2:
+            return
+        
+        # Fix size
+        header_lines.append(
+            self.get_line("word", [0x48:0x4C], "Fix Size")
+        )
+        
