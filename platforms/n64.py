51224c373c190f2ae929eaa4786e72c7251ac231
angie <angheloalf95@gmail.com>
2022-10-07 21:25:53 -0300

    Add libultra symbols

--- platforms/n64.py
+++ platforms/n64.py
@@ -3,3 +3,6 @@ from util import compiler, log, options, palettes, symbols
 
 def init(target_bytes: bytes):
     symbols.spim_context.fillDefaultBannedSymbols()
+
+    symbols.spim_context.globalSegment.fillLibultraSymbols()
+    symbols.spim_context.globalSegment.fillHardwareRegs(True)
