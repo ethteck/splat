2cc14a8b3b5b274d6e4727a907108e940a66fd09
angie <angheloalf95@gmail.com>
2022-11-29 13:46:16 -0300

    Allow changing include_macro_inc in options.py

--- util/options.py
+++ util/options.py
@@ -131,6 +131,8 @@ class SplatOpts:
     asm_data_macro: str
     # Determines the macro used at the end of a function, such as endlabel or .end
     asm_end_label: str
+    # Determines including the macro.inc file on non-migrated rodata variables
+    include_macro_inc: bool
     # Determines the number of characters to left align before the TODO finish documenting
     mnemonic_ljust: int
     # Determines whether to pad the rom address
@@ -351,6 +353,7 @@ def _parse_yaml(
         asm_jtbl_label_macro=p.parse_opt("asm_jtbl_label_macro", str, comp.asm_jtbl_label_macro),
         asm_data_macro=p.parse_opt("asm_data_macro", str, comp.asm_data_macro),
         asm_end_label=p.parse_opt("asm_end_label", str, comp.asm_end_label),
+        include_macro_inc=p.parse_opt("include_macro_inc", bool, comp.include_macro_inc),
         mnemonic_ljust=p.parse_opt("mnemonic_ljust", int, 11),
         rom_address_padding=p.parse_opt("rom_address_padding", bool, False),
         mips_abi_gpr=p.parse_opt_within(
