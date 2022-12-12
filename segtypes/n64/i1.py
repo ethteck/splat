320aa7126744394dcee039d5075223fd7a8bc4c5
MegaMech <MegaMech@users.noreply.github.com>
2022-12-11 21:10:09 -0700

    Update i1.py

--- segtypes/n64/i1.py
+++ segtypes/n64/i1.py
@@ -5,5 +5,5 @@ from segtypes.n64.img import N64SegImg
 
 class N64SegI1(N64SegImg):
     def __init__(self, *args, **kwargs):
-        kwargs["img_cls"] = n64img.image.IA1
+        kwargs["img_cls"] = n64img.image.I1
         super().__init__(*args, **kwargs)
