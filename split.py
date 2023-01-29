74dd721932c0a10258ec186451a401c87263ff59
Anghelo Carvajal <angheloalf95@gmail.com>
2023-01-29 11:14:28 -0300

    Warn if migrated rodata symbols have gaps (#211)
    * Warn if migrated rodata symbols have gaps
    
    * black
    
    * mypy
    
    * version bump and changelog

--- split.py
+++ split.py
@@ -17,7 +17,7 @@ from segtypes.linker_entry import LinkerWriter, to_cname
 from segtypes.segment import Segment
 from util import compiler, log, options, palettes, symbols, relocs
 
-VERSION = "0.12.11"
+VERSION = "0.12.12"
 # This value should be keep in sync with the version listed on requirements.txt
 SPIMDISASM_MIN = (1, 10, 0)
 
