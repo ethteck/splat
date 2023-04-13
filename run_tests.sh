feb626065260c355dee6a06eac1acd77b4ea2bf9
sozud <122322823+sozud@users.noreply.github.com>
2023-04-12 23:40:05 -0400

    Add rm

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,6 @@ cd test/basic_app && sh clean.sh && cd ../../ \
 # pull compiler container
 docker pull murachue/toolchain64:o32 && \
 # compile the app
-docker run -t  -w /basic_app -v $(pwd)/test/basic_app:/basic_app murachue/toolchain64:o32 sh -c "cd /basic_app && sh build.sh" && \
+docker run -t --rm -w /basic_app -v $(pwd)/test/basic_app:/basic_app murachue/toolchain64:o32 sh -c "cd /basic_app && sh build.sh" && \
 # split and compare
 python3 test.py
