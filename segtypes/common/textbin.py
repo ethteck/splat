34608a5b42191d926be33685eb218bdaf8373321
angie <angheloalf95@gmail.com>
2023-08-29 13:31:10 -0400

    rodata bin

--- segtypes/common/textbin.py
+++ segtypes/common/textbin.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Optional
+from typing import Optional, TextIO
 
 from util import log, options
 
@@ -27,17 +27,10 @@ class CommonSegTextbin(CommonSegment):
 
         return options.opts.asset_path / self.dir / f"{self.name}.{typ}.bin"
 
-    def split(self, rom_bytes):
-        s_path = self.out_path()
-        assert s_path is not None
-        s_path.parent.mkdir(parents=True, exist_ok=True)
+
+    def write_bin(self, rom_bytes):
         binpath = self.bin_path()
         binpath.parent.mkdir(parents=True, exist_ok=True)
-
-        if self.rom_end is None:
-            log.error(
-                f"segment {self.name} needs to know where it ends; add a position marker [0xDEADBEEF] after it"
-            )
 
         assert isinstance(self.rom_start, int)
         assert isinstance(self.rom_end, int)
@@ -46,31 +39,58 @@ class CommonSegTextbin(CommonSegment):
 
         self.log(f"Wrote {self.name} to {binpath}")
 
+
+    def write_asm_contents(self, rom_bytes, f: TextIO):
+        binpath = self.bin_path()
+
+        assert isinstance(self.rom_start, int)
+        assert isinstance(self.rom_end, int)
+
+        f.write(f".section {self.get_linker_section()}")
+        section_flags = self.get_section_flags()
+        if section_flags:
+            f.write(f', "{section_flags}"')
+        f.write("\n\n")
+
+        # Check if there's a symbol at this address
+        sym = None
+        vram = self.rom_to_ram(self.rom_start)
+        if vram is not None:
+            sym = self.get_symbol(vram, in_segment=True)
+
+        if sym is not None:
+            f.write(f"{options.opts.asm_function_macro} {sym.name}\n")
+
+        f.write(f'.incbin "{binpath}"\n')
+
+        if sym is not None and sym.given_name_end is not None:
+            f.write(f"{options.opts.asm_function_macro} {sym.given_name_end}\n")
+
+
+    def split(self, rom_bytes):
+        if self.rom_end is None:
+            log.error(
+                f"segment {self.name} needs to know where it ends; add a position marker [0xDEADBEEF] after it"
+            )
+
+        self.write_bin(rom_bytes)
+
+        s_path = self.out_path()
+        assert s_path is not None
+        s_path.parent.mkdir(parents=True, exist_ok=True)
+
         with s_path.open("w") as f:
             f.write('.include "macro.inc"\n\n')
             preamble = options.opts.generated_s_preamble
             if preamble:
                 f.write(preamble + "\n")
 
-            f.write(f".section {self.get_linker_section()}")
-            section_flags = self.get_section_flags()
-            if section_flags:
-                f.write(f', "{section_flags}"')
-            f.write("\n\n")
+            self.write_asm_contents(rom_bytes, f)
 
-            # Check if there's a symbol at this address
-            sym = None
-            vram = self.rom_to_ram(self.rom_start)
-            if vram is not None:
-                sym = self.get_symbol(vram, in_segment=True)
-
-            if sym is not None:
-                f.write(f"{options.opts.asm_function_macro} {sym.name}\n")
-
-            f.write(f'.incbin "{binpath}"\n')
-
-            if sym is not None and sym.given_name_end is not None:
-                f.write(f"{options.opts.asm_function_macro} {sym.given_name_end}\n")
+            # We check against CommonSegTextbin instead of the specific type because the other incbins inherit from this class
+            if isinstance(self.rodata_sibling, CommonSegTextbin):
+                f.write("\n")
+                self.rodata_sibling.write_asm_contents(rom_bytes, f)
 
     def should_scan(self) -> bool:
         return (
