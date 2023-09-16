e58e76b4bde626740dd3097b06f05f83fed3bb7c
Ethan Roseman <ethteck@gmail.com>
2023-09-17 05:12:40 +0900

    cleanup

--- disassembler/spimdisasm_disassembler.py
+++ disassembler/spimdisasm_disassembler.py
@@ -108,8 +108,7 @@ class SpimdisasmDisassembler(disassembler.Disassembler):
         )
 
         if opts.compiler == compiler.GCC and opts.platform == "ps2":
-            spimdisasm.common.GlobalConfig.DISASSEMBLE_UNKNOWN_INSTRUCTIONS = True
-        rabbitizer.config.toolchainTweaks_treatJAsUnconditionalBranch = False
+            rabbitizer.config.toolchainTweaks_treatJAsUnconditionalBranch = False
 
     def check_version(self, skip_version_check: bool, splat_version: str):
         if not skip_version_check and spimdisasm.__version_info__ < self.SPIMDISASM_MIN:
