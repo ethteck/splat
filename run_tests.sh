816b27bcee17ccd28649a92ef07b972ffd895ff6
Angie <angheloalf95@gmail.com>
2023-11-26 14:36:51 -0300

    yeet build.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -e
+
 # docker build container
 docker build --tag splat-build:latest . && \
 # get compilers and tools
 # clean
 make -C test/basic_app clean && \
 # build
-docker run --rm -v $(pwd):/splat -w /splat/test/basic_app splat-build sh build.sh && \
+docker run --rm -v $(pwd):/splat -w /splat/test/basic_app splat-build make -C test/basic_app all && \
 # test
 python3 test.py
--- test_gen_expected.sh
+++ test_gen_expected.sh
@@ -1,3 +1,3 @@
-docker run --rm -it -v $(pwd):/splat -w /splat/test/basic_app splat-build sh build.sh && \
+docker run --rm -it -v $(pwd):/splat -w /splat/test/basic_app splat-build make -C test/basic_app all && \
 rm -rf $(pwd)/test/basic_app/expected && \
 cp -r $(pwd)/test/basic_app/split $(pwd)/test/basic_app/expected
