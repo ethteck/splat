ac3823e63c5334e2671108603d8eb762f79cc5a3
angie <angheloalf95@gmail.com>
2023-12-24 13:43:42 -0300

    Implement pad segment

---
+++ segtypes/common/pad.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from segtypes.common.segment import CommonSegment
+from segtypes.linker_entry import LinkerEntry, LinkerWriter
+from segtypes.segment import Segment
+
+
+class LinkerEntryPad(LinkerEntry):
+    def __init__(
+        self,
+        segment: Segment,
+    ):
+        super().__init__(segment, [], Path(), "pad", "pad", False)
+        self.object_path = None
+
+    def emit_entry(self, linker_writer: LinkerWriter):
+        linker_writer._writeln(f". += 0x{self.segment.size:X};")
+
+
+class CommonSegPad(CommonSegment):
+    def get_linker_entries(self):
+        return [LinkerEntryPad(self)]
