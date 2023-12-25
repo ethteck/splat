4f157188ed3b32a2af24328ce4844438de46988f
angie <angheloalf95@gmail.com>
2023-12-25 12:39:14 -0300

    move split.py contents to scripts/split.py

--- src/splat/__init__.py
+++ src/splat/__init__.py
@@ -8,3 +8,5 @@ from . import util as util
 from . import disassembler as disassembler
 from . import platforms as platforms
 from . import segtypes as segtypes
+
+from . import scripts as scripts
---
+++ src/splat/scripts/__init__.py
@@ -0,0 +1,1 @@
+from . import split as split
