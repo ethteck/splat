43ad4abafea7c511b833ea896eb09f8b89ed395b
berendbutje <dont@me.dev>
2024-01-02 19:33:06 +0100

    Remove unnessecary printing

--- src/splat/disassembler/wasm_disassembler.py
+++ src/splat/disassembler/wasm_disassembler.py
@@ -3,12 +3,13 @@ from ..util import log, compiler
 from ..util.options import SplatOpts
 from typing import Set
 
+# Just a dummy class as it is not used yet.
 class WasmDisassembler(disassembler.Disassembler):
     def configure(self, opts: SplatOpts):
-        print("Configuring WASM Disassembler!")
+        pass
 
     def check_version(self, skip_version_check: bool, splat_version: str):
-        print("Version check WASM Disassembler!")
+        pass
 
     def known_types(self) -> Set[str]:
-        print("Known types WASM Disassembler!")
+        pass
