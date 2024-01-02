1ed76fb1152a55d26a25250260a9768e8ece6fe6
berendbutje <dont@me.dev>
2024-01-02 19:26:15 +0100

    Add dummy `asm` segment for platform `wasm`

--- src/splat/segtypes/wasm/asm.py
+++ src/splat/segtypes/wasm/asm.py
@@ -1,4 +1,31 @@
+from pathlib import Path
+from typing import Optional
+
+from ...util import options
+
 from ..common.bin import CommonSegBin
 
+from wasm_tob import (
+    decode_bytecode,
+    format_instruction,
+    INSN_ENTER_BLOCK,
+    INSN_LEAVE_BLOCK,
+)
+
 class WasmSegAsm(CommonSegBin):
-    pass
+    @staticmethod
+    def is_text() -> bool:
+        return True
+
+    def out_path(self) -> Optional[Path]:
+        return options.opts.asm_path / self.dir / f"{self.name}.wat"
+
+    def split(self, rom_bytes: bytes):
+        raw = rom_bytes[self.rom_start:self.rom_end]
+        
+        out_path = self.out_path()
+        if out_path:
+            out_path.parent.mkdir(parents=True, exist_ok=True)
+
+            with open(out_path, 'wb') as f:
+                f.write(raw) # syke
