eac3752eb6165d19acff72929cfcf85282034e12
berendbutje <dont@me.dev>
2024-01-04 11:27:40 +0100

    "Simplified" code for `func_type_to_wat`

--- src/splat/segtypes/wasm/wat.py
+++ src/splat/segtypes/wasm/wat.py
@@ -9,14 +9,14 @@ from wasm_tob import (
 
 
 def func_type_to_wat(index: int, func: FuncType) -> str:
-    params = ""
-    if func.param_count > 0:
-        vals = " ".join(map(format_lang_type, func.param_types))
-        params += f" (param {vals})"
-
-    returns = ""
-    if func.return_count:
-        returns += f" (result {format_lang_type(func.return_type)})"
+    params = (
+        f" (param {' '.join(map(format_lang_type, func.param_types))})"
+        if func.param_count
+        else ""
+    )
+    returns = (
+        f" (result {format_lang_type(func.return_type)})" if func.return_count else ""
+    )
 
     return f"(type (;{index};) (func{params}{returns}))"
 
