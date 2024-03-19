ac64daf2236dbe9cf80fd198336f3fcff403a862
angie <angheloalf95@gmail.com>
2024-03-19 10:15:31 -0300

    yeet GCC check

--- src/splat/platforms/ps2.py
+++ src/splat/platforms/ps2.py
@@ -4,7 +4,6 @@ import rabbitizer
 from ..util import compiler, options
 
 def init(target_bytes: bytes):
-    if options.opts.compiler == compiler.GCC:
-        rabbitizer.config.toolchainTweaks_treatJAsUnconditionalBranch = False
+    rabbitizer.config.toolchainTweaks_treatJAsUnconditionalBranch = False
 
     spimdisasm.common.GlobalConfig.ABI = spimdisasm.common.Abi.EABI64
