fc6708e7464f92b7ff3dfd5097d7186dd78a2e75
angie <angheloalf95@gmail.com>
2024-03-19 10:11:13 -0300

    Set EABI64 for PS2

--- src/splat/platforms/ps2.py
+++ src/splat/platforms/ps2.py
@@ -1,3 +1,4 @@
+import spimdisasm
 import rabbitizer
 
 from ..util import compiler, options
@@ -5,3 +6,5 @@ from ..util import compiler, options
 def init(target_bytes: bytes):
     if options.opts.compiler == compiler.GCC:
         rabbitizer.config.toolchainTweaks_treatJAsUnconditionalBranch = False
+
+    spimdisasm.common.GlobalConfig.ABI = spimdisasm.common.Abi.EABI64
