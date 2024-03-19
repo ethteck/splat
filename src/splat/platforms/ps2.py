df66a97ff270983c4ee381d5b431ce25e15e20cb
Anghelo Carvajal <angheloalf95@gmail.com>
2024-03-19 10:27:54 -0300

    Set ABI for ps2 (#359)
    * Move ps2 spimdisasm options to platforms/ps2.py
    
    * Set EABI64 for PS2
    
    * yeet GCC check
    
    * black

--- src/splat/platforms/ps2.py
+++ src/splat/platforms/ps2.py
@@ -1,2 +1,10 @@
+import spimdisasm
+import rabbitizer
+
+from ..util import compiler, options
+
+
 def init(target_bytes: bytes):
-    pass
+    rabbitizer.config.toolchainTweaks_treatJAsUnconditionalBranch = False
+
+    spimdisasm.common.GlobalConfig.ABI = spimdisasm.common.Abi.EABI64
