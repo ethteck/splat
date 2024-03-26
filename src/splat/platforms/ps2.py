35b2a5f9a7187dbcb526d7f7a149a5c2809aeacf
Angie <angheloalf95@gmail.com>
2024-03-26 16:47:40 -0300

    `SYMBOL_ALIGNMENT_REQUIRES_ALIGNED_SECTION`

--- src/splat/platforms/ps2.py
+++ src/splat/platforms/ps2.py
@@ -8,3 +8,4 @@ def init(target_bytes: bytes):
     rabbitizer.config.toolchainTweaks_treatJAsUnconditionalBranch = False
 
     spimdisasm.common.GlobalConfig.ABI = spimdisasm.common.Abi.EABI64
+    spimdisasm.common.GlobalConfig.SYMBOL_ALIGNMENT_REQUIRES_ALIGNED_SECTION = True
