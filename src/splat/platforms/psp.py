69a192c52a89d737d0dbe0e322eaafb7bc77334a
angie <angheloalf95@gmail.com>
2024-05-22 17:59:28 -0400

    Turn off symbol filtering on psp and ps2

--- src/splat/platforms/psp.py
+++ src/splat/platforms/psp.py
@@ -1,5 +1,8 @@
+import spimdisasm
 import rabbitizer
 
 
 def init(target_bytes: bytes):
     rabbitizer.config.toolchainTweaks_treatJAsUnconditionalBranch = False
+    spimdisasm.common.GlobalConfig.SYMBOL_FINDER_FILTER_LOW_ADDRESSES = False
+    spimdisasm.common.GlobalConfig.SYMBOL_FINDER_FILTER_HIGH_ADDRESSES = False
