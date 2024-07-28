5812a1e204cc6765cad8c6c4a481e8f271621601
Ethan Roseman <ethteck@gmail.com>
2024-07-28 16:57:56 +0900

    Stop creating empty asm dirs (#387)
    * Stop creating empty asm dirs
    
    * try to fix test

--- src/splat/__init__.py
+++ src/splat/__init__.py
@@ -3,7 +3,7 @@ from typing import Tuple
 __package_name__ = __name__
 
 # Should be synced with pyproject.toml
-__version__ = "0.24.6"
+__version__ = "0.24.7"
 __author__ = "ethteck"
 
 from . import util as util
