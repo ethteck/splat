ead1bace02db83fcb124bf861f695bcb465f6f41
Anghelo Carvajal <angheloalf95@gmail.com>
2025-02-23 12:02:12 -0300

    Enable `ld_dependencies` and `create_asm_dependencies` on the basic_app yaml
    Also reduce some paths and change compiler to KMC

--- test/test_gen_expected.sh
+++ test/test_gen_expected.sh
@@ -7,7 +7,9 @@ export SPIMDISASM_ASM_GENERATED_BY="False"
 # Ensure we start from a clean state
 rm -rf test/basic_app/split
 
-python3 ./split.py test/basic_app/splat.yaml --use-cache
+cd test/basic_app
+python3 -m splat split splat.yaml --use-cache
+cd ../..
 
 rm -rf test/basic_app/expected
 cp -r test/basic_app/split test/basic_app/expected
