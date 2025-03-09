d1164d14d022add48cdfd3000b9c6bc722cea10b
Anghelo Carvajal <angheloalf95@gmail.com>
2025-03-09 14:21:07 -0300

    Disable gpRelHack

--- src/splat/segtypes/common/c.py
+++ src/splat/segtypes/common/c.py
@@ -277,7 +277,12 @@ class CommonSegC(CommonSegCodeSubsegment):
                             )
 
             if options.opts.keep_complete_c_asm_splits_for_objdiff_compatibility:
+                # Disable gpRelHack since this file is expected to be built with modern gas
+                section = self.spim_section.get_section()
+                old_value = section.gpRelHack
+                section.gpRelHack = False
                 self.split_as_asm_file(self.asm_out_path())
+                section.gpRelHack = old_value
 
     def get_c_preamble(self):
         ret = []
