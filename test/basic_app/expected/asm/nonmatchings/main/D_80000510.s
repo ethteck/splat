0aba0d484a763787a97cf8611a5bf4b9cc92dae8
Anghelo Carvajal <angheloalf95@gmail.com>
2025-09-04 13:19:35 -0400

    Update tests because latest spimdisasm output is slightly different

--- test/basic_app/expected/asm/nonmatchings/main/D_80000510.s
+++ test/basic_app/expected/asm/nonmatchings/main/D_80000510.s
@@ -1,7 +1,8 @@
 .section .rodata
 
-dlabel D_80000510
 nonmatching D_80000510
+
+dlabel D_80000510
     /* 1110 80000510 00010203 */ .word 0x00010203
     /* 1114 80000514 04050607 */ .word 0x04050607
 enddlabel D_80000510
