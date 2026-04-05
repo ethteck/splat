04770330a16defb62f00326adb531ecb700f3a94
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2026-04-05 19:00:10 +0200

    Fix test

--- test/basic_app/expected/basic_app.d
+++ test/basic_app/expected/basic_app.d
@@ -4,18 +4,10 @@ build/basic_app_target.elf: \
     build/src/main.o \
     build/asm/handwritten.o \
     build/asm/data/main.data.o \
-    build/asm/handwritten.o \
-    build/src/main.o \
-    build/asm/handwritten.o \
-    build/asm/data/main.bss.o \
-    build/asm/handwritten.o
+    build/asm/data/main.bss.o
 build/asm/header.o:
 build/assets/dummy_ipl3.o:
 build/src/main.o:
 build/asm/handwritten.o:
 build/asm/data/main.data.o:
-build/asm/handwritten.o:
-build/src/main.o:
-build/asm/handwritten.o:
 build/asm/data/main.bss.o:
-build/asm/handwritten.o:
