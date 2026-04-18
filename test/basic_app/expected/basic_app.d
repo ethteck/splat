f3a58d2d42470e1f032392d0675e1110927d315b
ser-pounce <655122+ser-pounce@users.noreply.github.com>
2026-04-18 07:21:29 +0200

    Partial test fix

--- test/basic_app/expected/basic_app.d
+++ test/basic_app/expected/basic_app.d
@@ -11,3 +11,4 @@ build/src/main.o:
 build/asm/handwritten.o:
 build/asm/data/main.data.o:
 build/asm/data/main.bss.o:
+-include build/asm/header.d build/assets/dummy_ipl3.d build/src/main.d build/asm/handwritten.d build/asm/data/main.data.d build/asm/data/main.bss.d
