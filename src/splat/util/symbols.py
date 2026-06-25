140928dc47af25e450bf59d0e1546ab74d3b5601
Anghelo Carvajal <angheloalf95@gmail.com>
2026-06-24 22:33:07 -0400

    bump spimdisasm

--- src/splat/util/symbols.py
+++ src/splat/util/symbols.py
@@ -425,7 +425,7 @@ def initialize_spim_context(metadata_group: "SegmentMetadataGroup") -> None:
                 seg_meta.name,
             )
             for prioritized_seg in seg_meta.prioritized_segments:
-                spimdisasm_segment.addPrioritisedSegment(prioritized_seg)
+                spimdisasm_segment.addPrioritizedSegment(prioritized_seg)
 
             for sym in seg_meta.symbols.values():
                 add_symbol_to_spimdisasm_segment(spimdisasm_segment, sym)
